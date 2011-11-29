8e47ced71c5f950bba8005557c2db7bf51e79756
Bill Cromie <bill.cromie@gmail.com>
2011-11-29 16:52:16 -0500

    outputting

--- init.rb
+++ init.rb
@@ -5,8 +5,11 @@ require 'redistest.rb'
 r=Redis.new
 r.flushall
 rt=RedisTest.new
-pool = ThreadPool.new(threads = 100)
+pool = ThreadPool.new(threads = 10)
 (1...1000).each{|n|
-  pool.execute(n) {|local| rt.populate_user_fans_set(local) }
+  pool.execute(n) {|local| 
+    puts local
+    rt.populate_user_fans_set(local) 
+  }
 }
 pool.join
--- intersect.rb
+++ intersect.rb
@@ -1,10 +1,13 @@
 require 'rubygems'
 require 'thread_pool/lib/thread_pool.rb'
 require 'redistest.rb'
-
+r=Redis.new
 rt=RedisTest.new
 pool2 = ThreadPool.new(threads = 100)
-(1...1000).each{|n|
-  pool2.execute(n) {|local| rt.do_intersection(local) }
+r.smembers("userids").each{|n|
+  pool2.execute(n) {|local| 
+    puts local
+    rt.do_intersection(local) 
+  }
 }
 pool2.join
